48ac91f3020ff602da7fa1fb9c1f676fad52054d
drashti <riddhithekdi11111@gmail.com>
2021-09-03 17:30:21 +0530

    final

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(".icon").click(function(){
+       $("ul").slideToggle();
+})
+
+$('.owl-carousel').owlCarousel({
+       loop:true,
+       items:1,
+       dots:false,
+       nav:true,
+       smartSpeed:1500,
+       autoplay:true
+      
+     
+   });
